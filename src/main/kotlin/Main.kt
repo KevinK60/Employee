4068781353ac5ca6956c1c43a3c9c66b56d75ca8
20099267 <kevin9000m@Gmail.com>
2023-02-02 11:34:13 +0000

    String templates

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,54 +6,55 @@ fun main(args: Array<String>) {
 
 
     println("Payslip Printer")
-    printPaySlip()
 
-
-}
-
-fun printPaySlip() {
-    val df = DecimalFormat("#.##")
-    val decimal = BigDecimal(3.14159265359).setScale(2, RoundingMode.HALF_EVEN)
-    df.roundingMode = RoundingMode.CEILING
-        val firstName = "Joe"
-        val Surname = "Soap"
-        val gender = "m"
-        val employeeId = 6143
-        val grossSalary = 67543.21
-        val payePercentage = 38.5
-        val prsiPercentage = 5.2
-        val annualBonus = 1450.50
-        val cycleToWorkScheme = 54.33
-
-        fun culculatePaye(): Double {
-            return (grossSalary / 12 * payePercentage / 100)
-        }
-
-        fun culculateprsi(): Double {
-            return grossSalary / 12 * prsiPercentage / 100
-        }
-    fun totalreduction(): Double {
-        return culculateprsi() + culculateprsi() + cycleToWorkScheme
-    }
-    fun grosssalary(): Double {
-        return (grossSalary / 12) + (annualBonus / 12)
-    }
-        println("--------------------------------------------------------------------------------")
-        println("-                                 Monthly Payslip                              -")
-        println("--------------------------------------------------------------------------------")
-        println("-   Employee Name: ${firstName.uppercase()} ${Surname.uppercase()}(${gender.uppercase()}) Employee ID: $employeeId                               -")
-        println("--------------------------------------------------------------------------------")
-        println("------- PAYMENT DETAILS ---------------------DEDUCTION DETAILS------------------")
-        println()
-        println("-  Salary: " + BigDecimal(grossSalary / 12).setScale(2,RoundingMode.HALF_EVEN) + "                     PAYE: ${BigDecimal(culculatePaye()).setScale(2,RoundingMode.HALF_EVEN)}      -")
-
-        println("-  Bonus: " + BigDecimal(annualBonus / 12).setScale(2,RoundingMode.HALF_EVEN) + "                           PRSI: ${BigDecimal(culculateprsi()).setScale(2,RoundingMode.HALF_EVEN)}           -")
-        println("-                                       Cycle to work: $cycleToWorkScheme            -")
-        println("-----------------------------------------------------------------------------------")
-        println(" Gross pay: " + BigDecimal(grosssalary()).setScale(2,RoundingMode.HALF_EVEN) + "                 total reductions " + BigDecimal(totalreduction()).setScale(2,RoundingMode.HALF_EVEN))
-           println("-----------------------------------------------------------------------------")
-                              println("                       Net pay: " + BigDecimal(grosssalary() - totalreduction()).setScale(2,RoundingMode.HALF_EVEN))
-    println("--------------------------------------------------------------------------------")
+    println(printPaySlip1())
 
 }
+val df = DecimalFormat("#.##")
+val decimal = BigDecimal(3.14159265359).setScale(2, RoundingMode.HALF_EVEN)
+val firstName = "Joe"
+val Surname = "Soap"
+val gender = "m"
+val employeeId = 6143
+val grossSalary = 67543.21
+val payePercentage = 38.5
+val prsiPercentage = 5.2
+val annualBonus = 1450.50
+val cycleToWorkScheme = 54.33
 
+
+
+fun culculatePaye(): Double {
+    return (grossSalary / 12 * payePercentage / 100)
+}
+
+fun culculateprsi(): Double {
+    return grossSalary / 12 * prsiPercentage / 100
+}
+
+fun totalreduction(): Double {
+    return culculateprsi() + culculateprsi() + cycleToWorkScheme
+}
+
+fun grosssalary(): Double {
+    return (grossSalary / 12) + (annualBonus / 12)
+}
+ fun printPaySlip1() = """
+  --------------------------------------------------------------------------------
+  -                                                                              -
+  -                                Monthly Payslip                               -
+  --------------------------------------------------------------------------------
+  - Employee Name: ${firstName.uppercase()} ${Surname.uppercase()}(${gender.uppercase()}                Employee ID: $employeeId                               
+  -                                                                              -
+  -------- PAYMENT DETAILS ---------------------DEDUCTION DETAILS-----------------
+  -                                                                              -
+  -  Salary:${BigDecimal(grossSalary / 12).setScale(2,RoundingMode.HALF_EVEN)}                      PAYE: ${BigDecimal(culculatePaye()).setScale(2,RoundingMode.HALF_EVEN)}      -")
+  -                                                                              -
+  -  Bonus: ${BigDecimal(annualBonus / 12).setScale(2,RoundingMode.HALF_EVEN)}                      PRSI: ${BigDecimal(culculateprsi()).setScale(2,RoundingMode.HALF_EVEN)}           -")
+  -                                    Cycle to work: $cycleToWorkScheme          -
+  ---------------------------------------------------------------------------------
+  - Gross pay:  ${BigDecimal(grosssalary()).setScale(2,RoundingMode.HALF_EVEN)}                         total reductions  ${BigDecimal(totalreduction()).setScale(2,RoundingMode.HALF_EVEN)}
+  ---------------------------------------------------------------------------------
+                                      Net pay: " + ${BigDecimal(grosssalary() - totalreduction()).setScale(2,RoundingMode.HALF_EVEN)}
+  ---------------------------------------------------------------------------------")
+"""
